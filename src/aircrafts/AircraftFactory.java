e826eb74ae9cc1accf441f81591fef1477ca334a
Malebo MTHOMBENI <mmthombe@e4r9p10.wethinkcode.co.za>
2018-07-10 18:04:33 +0200

    second

---
+++ src/aircrafts/AircraftFactory.java
@@ -0,0 +1,9 @@
+package aircrafts;
+
+import interfaces.Flyable;
+
+public class AircraftFactory {
+    public static Flyable newAircraft(String type, Stirng name, int longitude, int latitude, int height){
+
+    }
+}
---
+++ src/weather/WeatherProvider.java
@@ -0,0 +1,20 @@
+package weather;
+
+public class WeatherProvider {
+    private static WeatherProvider weatherProvider;
+    private static String weather;
+
+    private WeatherProvider(){
+
+    }
+
+    public static WeatherProvider getProvider(){
+
+
+    }
+
+    public String getCurrentWeather(Coordinates coordinates){
+
+    }
+
+}
